121f1d65abd844385c3cb586e67dac09a21e8c69
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-04-17 15:34:37 -0300

    removed canEverTick from the grenade launcher

--- CoopGame/Source/CoopGame/Private/SGrenadeLauncher.cpp
+++ CoopGame/Source/CoopGame/Private/SGrenadeLauncher.cpp
@@ -10,8 +10,6 @@
 
 ASGrenadeLauncher::ASGrenadeLauncher()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 }
 
 void ASGrenadeLauncher::fire()
