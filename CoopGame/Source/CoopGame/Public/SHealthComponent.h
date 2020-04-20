3146bee45b7273e4de564b6f1edb63fdc467904b
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-04-20 16:59:27 -0300

    created a custom event to handle the health change.

--- CoopGame/Source/CoopGame/Public/SHealthComponent.h
+++ CoopGame/Source/CoopGame/Public/SHealthComponent.h
@@ -7,6 +7,21 @@
 #include "SHealthComponent.generated.h"
 
 
+//this is the onHealthChanged event
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FOnHealthChangedSignature,/*name of the event*/
+									USHealthComponent*,/*type of who trigger the event*/
+									trigger,/*name of the trigger*/
+									float,/*type of variable given*/
+									health,/*name of the variable given*/
+									float,/*type of variable given*/
+									healthDelta,
+									const class UDamageType*,
+									DamageType,
+									class AController*,
+									InstigatedBy,
+									AActor*,
+									DamageCauser)
+
 UCLASS( ClassGroup=(COOP), meta=(BlueprintSpawnableComponent) )
 class COOPGAME_API USHealthComponent : public UActorComponent
 {
@@ -26,6 +41,12 @@ protected:
 	float currentHealth;
 
 	UFUNCTION()//it's an event handler
-	void damageTakerHandle(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
-		
+	void damageTakerHandle(AActor* DamagedActor,
+							float Damage,
+							const class UDamageType* DamageType,
+							class AController* InstigatedBy,
+							AActor* DamageCauser);
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHealthChangedSignature onhealthChanged;
 };
