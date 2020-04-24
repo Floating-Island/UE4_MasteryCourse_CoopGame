7917687dc522e33f953c16dddb0596596795de61
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-04-24 10:16:13 -0300

    typo fixed.

--- CoopGame/Source/CoopGame/Ai/STrackerBot.h
+++ CoopGame/Source/CoopGame/Ai/STrackerBot.h
@@ -19,7 +19,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleDefaultsOnly, Category = "Components");
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
 	UStaticMeshComponent* meshComp;
 
 public:	
