d555e9acb906154f2937e807f88486bfcbd30fea
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-04-30 12:12:57 -0300

    Used the EQS to test distance to SCharacters.

--- CoopGame/Intermediate/Build/Win64/UE4Editor/Inc/CoopGame/CoopGame.init.gen.cpp
+++ CoopGame/Intermediate/Build/Win64/UE4Editor/Inc/CoopGame/CoopGame.init.gen.cpp
@@ -25,8 +25,8 @@ void EmptyLinkFunctionForGeneratedCodeCoopGame_init() {}
 				SingletonFuncArray,
 				ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0xBE2962BE,
-				0xDC4CFA2C,
+				0x458C24DD,
+				0xCE5CC6F5,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
