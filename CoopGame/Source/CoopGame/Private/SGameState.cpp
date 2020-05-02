8f28f310542009abed0d14fa43aba1de53871590
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-05-02 11:48:07 -0300

    added get replicated props method to allow game state's variables replication.

--- CoopGame/Source/CoopGame/Private/SGameState.cpp
+++ CoopGame/Source/CoopGame/Private/SGameState.cpp
@@ -3,7 +3,17 @@
 
 #include "SGameState.h"
 
+#include "Net/UnrealNetwork.h"
+
 void ASGameState::waveStateReplication(EWaveState oldWaveState)
 {
 	waveStateChanged(waveState, oldWaveState);
+}
+
+
+void ASGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASGameState, waveState);
 }
